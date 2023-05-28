7b25b26a9083f3234c88dbb8581c3120ac6acafc
AkagawaTsurunaki <965145619@qq.com>
2023-05-28 18:27:25 +0800

    Initial commit

---
+++ src/test/java/com/github/akagawatsurunaki/ankeito/AnkeitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.akagawatsurunaki.ankeito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnkeitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
