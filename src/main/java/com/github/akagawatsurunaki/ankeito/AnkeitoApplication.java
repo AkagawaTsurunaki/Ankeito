d73a20e3f222af5e2667b7a59b9735c640380c11
AkagawaTsurunaki <965145619@qq.com>
2023-05-30 11:45:28 +0800

    feat: 小测试

--- src/main/java/com/github/akagawatsurunaki/ankeito/AnkeitoApplication.java
+++ src/main/java/com/github/akagawatsurunaki/ankeito/AnkeitoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AnkeitoApplication {
 
+	// 这个文件你不用管，跟实验无关
 	public static void main(String[] args) {
 		SpringApplication.run(AnkeitoApplication.class, args);
 	}
