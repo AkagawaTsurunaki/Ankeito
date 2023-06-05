a0bd6fa5a37ea5c27fa56f342b60d5bcf548380f
AkagawaTsurunaki <965145619@qq.com>
2023-06-05 19:21:15 +0800

    refactor: 删除无用代码

--- src/main/java/com/github/akagawatsurunaki/ankeito/common/enumeration/UserStatus.java
+++ src/main/java/com/github/akagawatsurunaki/ankeito/common/enumeration/UserStatus.java
@@ -1,9 +1,6 @@
 package com.github.akagawatsurunaki.ankeito.common.enumeration;
 
-import com.github.akagawatsurunaki.ankeito.entity.User;
 import lombok.AllArgsConstructor;
-
-import java.util.Arrays;
 
 @AllArgsConstructor
 public enum UserStatus {
@@ -12,16 +9,5 @@ public enum UserStatus {
 
     final int value;
     final String chinese;
-
-    /**
-     *
-     * 根据指定 value 值获取对应的 Enum
-     *
-     * @param value Enum 对应的 value
-     * @return 对应的 UserStatus, 如果不存在默认为 DISABLE
-     */
-    public static UserStatus getEnum(int value) {
-        return Arrays.stream(UserStatus.values()).filter( it -> it.value == value).findFirst().orElse(DISABLE);
-    }
 
 }
