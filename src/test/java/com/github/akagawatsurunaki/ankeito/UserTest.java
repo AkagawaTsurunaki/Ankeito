81719da558d44e72ead305f13cab6ac0ade0e60d
AkagawaTsurunaki <965145619@qq.com>
2023-06-05 21:22:12 +0800

    test: 测试 User 的 hasCode 和 equal

--- src/test/java/com/github/akagawatsurunaki/ankeito/UserTest.java
+++ src/test/java/com/github/akagawatsurunaki/ankeito/UserTest.java
@@ -53,4 +53,64 @@ public class UserTest {
 
     }
 
+    @Test
+    public void testEqualsAndHashCode() {
+        String id = "1234567890abcdef";
+        String username = "testUser";
+        String password = "testPassword";
+        Date startTime = new Date();
+        Date stopTime = new Date();
+        UserRole userRole = UserRole.ADMIN;
+        UserStatus userStatus = UserStatus.ENABLE;
+        String createdBy = "creator";
+        Date creationTime = new Date();
+        String lastUpdatedBy = "updater";
+        Date lastUpdateTime = new Date();
+
+        User user1 = User.builder()
+                .id(id)
+                .username(username)
+                .password(password)
+                .startTime(startTime)
+                .stopTime(stopTime)
+                .userRole(userRole)
+                .userStatus(userStatus)
+                .createdBy(createdBy)
+                .creationTime(creationTime)
+                .lastUpdatedBy(lastUpdatedBy)
+                .lastUpdateTime(lastUpdateTime)
+                .build();
+        User user2 = User.builder()
+                .id(id)
+                .username(username)
+                .password(password)
+                .startTime(startTime)
+                .stopTime(stopTime)
+                .userRole(userRole)
+                .userStatus(userStatus)
+                .createdBy(createdBy)
+                .creationTime(creationTime)
+                .lastUpdatedBy(lastUpdatedBy)
+                .lastUpdateTime(lastUpdateTime)
+                .build();
+        User user3 = User.builder()
+                .id("abcdef1234567890")
+                .username(username)
+                .password(password)
+                .startTime(startTime)
+                .stopTime(stopTime)
+                .userRole(userRole)
+                .userStatus(userStatus)
+                .createdBy(createdBy)
+                .creationTime(creationTime)
+                .lastUpdatedBy(lastUpdatedBy)
+                .lastUpdateTime(lastUpdateTime)
+                .build();
+
+        Assertions.assertEquals(user1, user2);
+        Assertions.assertNotEquals(user1, user3);
+        Assertions.assertEquals(user1.hashCode(), user2.hashCode());
+        Assertions.assertNotEquals(user1.hashCode(), user3.hashCode());
+    }
+
 }
