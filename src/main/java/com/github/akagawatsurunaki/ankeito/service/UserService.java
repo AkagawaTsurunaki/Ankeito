cd7904369cd777bbc8de04c953134d3afb8d80cf
AkagawaTsurunaki <965145619@qq.com>
2023-06-05 15:30:44 +0800

    feat: 实现增加一个用户的方法

--- src/main/java/com/github/akagawatsurunaki/ankeito/service/UserService.java
+++ src/main/java/com/github/akagawatsurunaki/ankeito/service/UserService.java
@@ -1,12 +1,14 @@
 package com.github.akagawatsurunaki.ankeito.service;
 
-import com.alibaba.fastjson2.JSONObject;
+import com.github.akagawatsurunaki.ankeito.api.dto.UserDTO;
 import com.github.akagawatsurunaki.ankeito.api.result.ServiceResult;
+import com.github.akagawatsurunaki.ankeito.common.convertor.UserConvertor;
 import com.github.akagawatsurunaki.ankeito.common.enumeration.ServiceResultCode;
 import com.github.akagawatsurunaki.ankeito.entity.User;
 import com.github.akagawatsurunaki.ankeito.mapper.UserMapper;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -27,6 +29,21 @@ public class UserService {
                 ServiceResultCode.OK,
                 "共查询到" + allUsers.size() + "条用户信息",
                 allUsers);
+    }
+
+    public ServiceResult<User> add(@NonNull UserDTO userDTO) {
+        val user = new UserConvertor(userDTO).dtoToEntity();
+        val insert = userMapper.insert(user);
+        if (insert == 1) {
+            return ServiceResult.of(
+                    ServiceResultCode.OK,
+                    "成功增加1条用户信息",
+                    user);
+        }
+        return ServiceResult.of(
+                ServiceResultCode.FAILED,
+                "增加用户信息失败"
+        );
     }
 
 }
