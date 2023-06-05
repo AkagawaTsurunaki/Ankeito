68894553f0bf8778bacdc3e47cc2aa1cccf0c2e3
AkagawaTsurunaki <965145619@qq.com>
2023-06-05 20:13:37 +0800

    test: 测试Controller

--- src/test/java/com/github/akagawatsurunaki/ankeito/AnkeitoApplicationTests.java
+++ src/test/java/com/github/akagawatsurunaki/ankeito/AnkeitoApplicationTests.java
@@ -1,8 +1,6 @@
 package com.github.akagawatsurunaki.ankeito;
 
-import com.alibaba.fastjson2.JSONObject;
 import com.github.akagawatsurunaki.ankeito.service.UserService;
-import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
