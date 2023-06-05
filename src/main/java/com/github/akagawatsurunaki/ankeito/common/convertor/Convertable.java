00787ee7957f892d159255d273b36c62a319421b
AkagawaTsurunaki <965145619@qq.com>
2023-06-05 14:43:26 +0800

    feat: 利用继承实现枚举的自转化

---
+++ src/main/java/com/github/akagawatsurunaki/ankeito/common/convertor/Convertable.java
@@ -0,0 +1,18 @@
+package com.github.akagawatsurunaki.ankeito.common.convertor;
+
+import com.github.akagawatsurunaki.ankeito.common.enumeration.UserStatus;
+import lombok.AllArgsConstructor;
+
+public class Convertable<E> {
+
+//
+//    static <E> E valueOf(int value) {
+//        for (UserStatus userStatus : UserStatus.values()) {
+//            if (userStatus.value == value){
+//                return userStatus;
+//            }
+//        }
+//        return null;
+//    }
+
+}
--- src/main/java/com/github/akagawatsurunaki/ankeito/common/enumeration/UserStatus.java
+++ src/main/java/com/github/akagawatsurunaki/ankeito/common/enumeration/UserStatus.java
@@ -4,8 +4,11 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public enum UserStatus {
-    ENABLE("启用"),
-    DISABLE("不启用");
+    ENABLE(1, "启用"),
+    DISABLE(0, "不启用");
 
-    final String value;
+    final int value;
+    final String chinese;
+
+
 }
