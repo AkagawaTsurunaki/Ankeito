1e074ee6475e9ef545912e7a466fd7d9955f2502
AkagawaTsurunaki <965145619@qq.com>
2023-06-05 15:30:05 +0800

    feat: 实现 UserDTO 到 User 实体类的转化器

--- src/main/java/com/github/akagawatsurunaki/ankeito/common/enumeration/UserStatus.java
+++ src/main/java/com/github/akagawatsurunaki/ankeito/common/enumeration/UserStatus.java
@@ -1,6 +1,9 @@
 package com.github.akagawatsurunaki.ankeito.common.enumeration;
 
+import com.github.akagawatsurunaki.ankeito.entity.User;
 import lombok.AllArgsConstructor;
+
+import java.util.Arrays;
 
 @AllArgsConstructor
 public enum UserStatus {
@@ -10,5 +13,15 @@ public enum UserStatus {
     final int value;
     final String chinese;
 
+    /**
+     *
+     * 根据指定 value 值获取对应的 Enum
+     *
+     * @param value Enum 对应的 value
+     * @return 对应的 UserStatus, 如果不存在默认为 DISABLE
+     */
+    public static UserStatus getEnum(int value) {
+        return Arrays.stream(UserStatus.values()).filter( it -> it.value == value).findFirst().orElse(DISABLE);
+    }
 
 }
