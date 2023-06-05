e56445fbac06dda492fbbff302657cff05299a93
AkagawaTsurunaki <965145619@qq.com>
2023-06-05 21:31:35 +0800

    test: 测试ModifyUserParam

--- src/main/java/com/github/akagawatsurunaki/ankeito/api/param/ModifyUserParam.java
+++ src/main/java/com/github/akagawatsurunaki/ankeito/api/param/ModifyUserParam.java
@@ -2,12 +2,10 @@ package com.github.akagawatsurunaki.ankeito.api.param;
 
 import com.github.akagawatsurunaki.ankeito.common.enumeration.UserRole;
 import com.github.akagawatsurunaki.ankeito.common.enumeration.UserStatus;
-import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
-@Builder
 @EqualsAndHashCode(callSuper = false)
 public class ModifyUserParam extends AddUserParam{
     String id;
