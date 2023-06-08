6b6e3854a739935cd41eb60218eb49ce6a54079a
AkagawaTsurunaki <965145619@qq.com>
2023-06-08 13:48:09 +0800

    fix: 修复了查询结果为空的bug

--- src/main/resources/sql/ankeito_dev_nova.sql
+++ src/main/resources/sql/ankeito_dev_nova.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80029
  File Encoding         : 65001
 
- Date: 08/06/2023 13:34:44
+ Date: 08/06/2023 13:47:48
 */
 
 SET NAMES utf8mb4;
@@ -36,7 +36,10 @@ CREATE TABLE `project`  (
 -- ----------------------------
 -- Records of project
 -- ----------------------------
-INSERT INTO `project` VALUES ('fa952223-5e74-44c6-a3a9-f916c18b8fcf', 'TODO', '你好吗', '阿杜hi客户覅了电话哦', 'admin', 'admin', '2023-06-08 13:32:53', '2023-06-08 13:32:53');
+INSERT INTO `project` VALUES ('74f40751-938c-4972-90d5-b7a67964e40d', 'TODO', '关于苏联修正主义对我国的影响', '这是一份历史调查问卷', 'admin', 'admin', '2023-06-08 13:36:05', '2023-06-08 13:36:05');
+INSERT INTO `project` VALUES ('a73990ec-6277-4462-8e45-4bc687f27556', 'TODO', '你好吗', '第二个你好吗?', 'admin', 'admin', '2023-06-08 13:41:32', '2023-06-08 13:41:32');
+INSERT INTO `project` VALUES ('e358c2e1-1670-48f8-b1f9-02fa05e29df0', 'TODO', '大学生恋爱调查问卷', '这是一份关于大学生恋爱的调查问卷', 'admin', 'admin', '2023-06-08 13:36:13', '2023-06-08 13:36:13');
+INSERT INTO `project` VALUES ('fa952223-5e74-44c6-a3a9-f916c18b8fcf', 'TODO', '你好吗', '这是一份心理调查问卷', 'admin', 'admin', '2023-06-08 13:32:53', '2023-06-08 13:32:53');
 
 -- ----------------------------
 -- Table structure for user
