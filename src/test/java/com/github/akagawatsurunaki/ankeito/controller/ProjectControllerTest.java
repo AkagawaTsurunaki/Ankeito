b5f375ca10b6e6081baa513c0957cf5edb62848f
AkagawaTsurunaki <965145619@qq.com>
2023-06-15 20:15:03 +0800

    test: 根据测试结果修改原项目, 测试覆盖率96%

---
+++ src/test/java/com/github/akagawatsurunaki/ankeito/controller/ProjectControllerTest.java
@@ -0,0 +1,79 @@
+package com.github.akagawatsurunaki.ankeito.controller;
+
+import cn.hutool.core.util.RandomUtil;
+import com.github.akagawatsurunaki.ankeito.api.param.add.AddProjectParam;
+import com.github.akagawatsurunaki.ankeito.api.param.delete.DeleteProjectParam;
+import com.github.akagawatsurunaki.ankeito.api.param.modify.ModifyProjectParam;
+import com.github.akagawatsurunaki.ankeito.api.param.query.QueryProjectListParam;
+import com.github.akagawatsurunaki.ankeito.entity.Project;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class ProjectControllerTest {
+
+    private String projectName;
+    private String projectContent;
+    private String createdBy;
+    private String lastUpdatedBy;
+
+    @Autowired
+    private ProjectController projectController;
+
+    private void init() {
+        projectName = RandomUtil.randomString(RandomUtil.BASE_CHAR, 16);
+        projectContent = RandomUtil.randomString(RandomUtil.BASE_CHAR, 200);
+        createdBy = RandomUtil.randomString(RandomUtil.BASE_CHAR, 8);
+        lastUpdatedBy = RandomUtil.randomString(RandomUtil.BASE_CHAR, 8);
+    }
+
+    @Test
+    public void testAdd() {
+        val addProjectParam = new AddProjectParam();
+        addProjectParam.setProjectName(projectName);
+        addProjectParam.setProjectContent(projectContent);
+        addProjectParam.setLastUpdatedBy(lastUpdatedBy);
+        addProjectParam.setCreatedBy(createdBy);
+        projectController.addProjectInfo(addProjectParam);
+    }
+
+
+    @Test
+    public void testFindAndModify() {
+        var httpResponseEntity = projectController.queryProjectList(new QueryProjectListParam());
+        System.out.println("httpResponseEntity = " + httpResponseEntity);
+
+        val queryProjectListParam = new QueryProjectListParam();
+        queryProjectListParam.setProjectName(projectName);
+        httpResponseEntity = projectController.queryProjectList(queryProjectListParam);
+        val projects = ((List<Project>) httpResponseEntity.getData());
+        val project = projects.get(0);
+
+        projectController.queryProjectList(new QueryProjectListParam());
+
+        val modifyProjectParam = new ModifyProjectParam();
+        modifyProjectParam.setId(modifyProjectParam.getId());
+        modifyProjectParam.setProjectName(projectName);
+        modifyProjectParam.setProjectContent(RandomUtil.randomString(RandomUtil.BASE_CHAR, 200));
+        projectController.modifyProjectInfo(modifyProjectParam);
+    }
+
+    public void testDelete() {
+        val queryProjectListParam = new QueryProjectListParam();
+        queryProjectListParam.setProjectName(projectName);
+        val httpResponseEntity = projectController.queryProjectList(queryProjectListParam);
+        val projects = ((List<Project>) httpResponseEntity.getData());
+        val project = projects.get(0);
+
+        projectController.deleteProjectById(new DeleteProjectParam());
+
+        val deleteProjectParam = new DeleteProjectParam();
+        deleteProjectParam.setId(project.getId());
+
+        projectController.deleteProjectById(deleteProjectParam);
+    }
+}
