41e00d1f4fc8d4b339be0e5e6ca39dd2b90d2bdc
AkagawaTsurunaki <965145619@qq.com>
2023-06-21 20:01:20 +0800

    fix: 修复了无法找到主键的WARN

--- src/main/java/com/github/akagawatsurunaki/ankeito/entity/answer/ResponseOption.java
+++ src/main/java/com/github/akagawatsurunaki/ankeito/entity/answer/ResponseOption.java
@@ -1,15 +1,19 @@
 package com.github.akagawatsurunaki.ankeito.entity.answer;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class ResponseOption {
+    @TableId(type = IdType.INPUT)
     String responseSheetId;
     /**
      * id 和 questionId是选项的主键
      */
     Integer optionId;
     String questionId;
+    String qnnreId;
 }
--- src/main/java/com/github/akagawatsurunaki/ankeito/mapper/answer/ResponseOptionMapper.java
+++ src/main/java/com/github/akagawatsurunaki/ankeito/mapper/answer/ResponseOptionMapper.java
@@ -1,8 +1,9 @@
 package com.github.akagawatsurunaki.ankeito.mapper.answer;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.github.akagawatsurunaki.ankeito.entity.answer.ResponseOption;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface ResponseOptionMapper extends BaseMapper<ResponseOptionMapper> {
+public interface ResponseOptionMapper extends BaseMapper<ResponseOption> {
 }
