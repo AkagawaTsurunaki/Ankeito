20602e99678c36eebba4b8f646b045eb52f67079
AkagawaTsurunaki <965145619@qq.com>
2023-06-21 15:47:53 +0800

    feat: 新增实体类

---
+++ src/main/java/com/github/akagawatsurunaki/ankeito/entity/answer/ResponseOption.java
@@ -0,0 +1,15 @@
+package com.github.akagawatsurunaki.ankeito.entity.answer;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ResponseOption {
+    String responseSheetId;
+    /**
+     * id 和 questionId是选项的主键
+     */
+    Integer optionId;
+    String questionId;
+}
