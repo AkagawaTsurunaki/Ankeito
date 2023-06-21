996f3270708c5695e9d94c5bfd187e12350ca18e
AkagawaTsurunaki <965145619@qq.com>
2023-06-21 15:50:24 +0800

    feat: 根据实体类创建对应的mapper

---
+++ src/main/java/com/github/akagawatsurunaki/ankeito/mapper/answer/ResponseOptionMapper.java
@@ -0,0 +1,8 @@
+package com.github.akagawatsurunaki.ankeito.mapper.answer;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ResponseOptionMapper extends BaseMapper<ResponseOptionMapper> {
+}
