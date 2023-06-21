dbfb7b3a4d7b42073c246b786bff906d1eb91bc3
AkagawaTsurunaki <965145619@qq.com>
2023-06-22 00:51:31 +0800

    fix: 修复了前端radio可以被选择的错误

--- src/main/java/com/github/akagawatsurunaki/ankeito/mapper/answer/ResponseOptionMapper.java
+++ src/main/java/com/github/akagawatsurunaki/ankeito/mapper/answer/ResponseOptionMapper.java
@@ -1,9 +1,18 @@
 package com.github.akagawatsurunaki.ankeito.mapper.answer;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.github.akagawatsurunaki.ankeito.entity.answer.ResponseOption;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.lang.NonNull;
+
+import java.util.List;
 
 @Mapper
 public interface ResponseOptionMapper extends BaseMapper<ResponseOption> {
+
+    default List<ResponseOption> selectByResponseSheetId(@NonNull String responseSheetId) {
+        return selectList(new QueryWrapper<ResponseOption>().lambda().eq(ResponseOption::getResponseSheetId,
+                responseSheetId));
+    }
 }
