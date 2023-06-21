a2e8bea56c221a72937da4505685589ef7cb3c37
AkagawaTsurunaki <965145619@qq.com>
2023-06-22 00:02:56 +0800

    feat: 编写显示单选的js函数

--- src/main/resources/static/pages/responseSheetDetail/index.js
+++ src/main/resources/static/pages/responseSheetDetail/index.js
@@ -31,7 +31,24 @@ onload = () => {
                 }
                 responseSheetDTO.qnnreDTO = {
                     qnnre: res.data.qnnreDTO.qnnre,
-                    questionDTOList: res.data.qnnreDTO.questionDTOList
+                    questionDTOList: [{
+                        question: {
+                            id,
+                            qnnreId,
+                            content,
+                            required,
+                            type,
+                        },
+                        optionList: [{
+                            option: {
+                                id,
+                                questionId,
+                                content,
+                                qnnreId,
+                            },
+                            selected
+                        }],
+                    }] = res.data.qnnreDTO.questionDTOList
                 }
                 questionnaireId = responseSheetDTO.qnnreDTO.qnnre.id
                 questionnaireTitle = responseSheetDTO.qnnreDTO.qnnre.name
@@ -39,33 +56,36 @@ onload = () => {
                 $('#qnnreTitle').text(questionnaireTitle)
                 $('#qnnreDescription').text(questionnaireDescription)
                 $('#respondentName').text(responseSheetDTO.responseSheet.respondentName)
+                load()
             } else {
                 alert(res.message)
             }
         }
     });
+    //load()
+
 }
 
 const load = () => {
-
     responseSheetDTO.qnnreDTO.questionDTOList.forEach(questionDTO => {
-        let question = questionDTO.question
-        let ele
-        switch (question.type) {
+        switch (questionDTO.question.type) {
             case 'SINGLE_CHOICE_QUESTION':
-                ele = singleChoice(question)
+                singleChoice(questionDTO)
                 break;
-            case  'MULTIPLE_CHOICE_QUESTION':
-                ele = singleChoice(question)
+            case 'MULTIPLE_CHOICE_QUESTION':
+                singleM(questionDTO)
                 break;
             default:
                 break;
         }
-
     })
-
 }
 
+/**
+ * 增加一个单选题(已作答)
+ * @param questionDTO
+ * @author AkagawaTsurunaki
+ */
 const singleChoice = (questionDTO) => {
     let question = questionDTO.question
     let ele = `
@@ -80,46 +100,13 @@ const singleChoice = (questionDTO) => {
     </div>
     `
     $('#problem').append(ele)
-    questionDTO.optionList.map(item => {
-        $(`#question${problemIndex} .bottom2`).append(`
+    questionDTO.optionList.map(optionDTO => {
+        $(`#question${question.id} .bottom2`).append(`
       <div style="display: flex; align-items: center;">
         <label class="radio-inline">
-          <input type="radio">${item.chooseTerm ? item.chooseTerm : ''}
+          <input type="radio" ${optionDTO.selected ? 'checked' : ''}>${optionDTO.option.content}
         </label>
       </div>
     `)
     })
-
-
-}
-
-const handleAddSingleChoice = () => {
-    problem.type = 'SINGLE_CHOICE_QUESTION'
-    return `
-    <div class="question" id="question${problem.length}" data-type="1" data-problemIndex="${problem.length}">
-      <div class="top">
-        <span class="question-title" id="questionTitle">1.请编辑问题？</span>
-        <span class="must-answer" id="mustAnswer" onclick="onMustAnswerClick(${problem.length})">必答题</span>
-      </div>
-      <div class="bottom">
-        <textarea class="form-control textarea" id="problemName" placeholder="单选题目" rows="4" oninput="onInput(${problem.length}, ${undefined}, 'problemName')"></textarea>
-        <div class="option" id="option">
-          <div class="option-item" id="optionItem0">
-            <input type="text" class="form-control" id="chooseTerm" placeholder="选项【单选】" oninput="onInput(${problem.length}, 0, 'chooseTerm')" />
-            <span class="option-del" onclick="singleChoiceDelOption(${problem.length}, 0)">删除</span>
-          </div>
-        </div>
-        <div>
-          <button type="button" class="btn btn-link btn-add-option" onclick="singleChoiceAddOption(${problem.length})">添加选项</button>
-        </div>
-        <div class="btn-group">
-          <button type="button" id="cancelEdit" class="btn btn-default" onclick="cancelEdit(${problem.length})">取消编辑</button>
-          <button type="button" id="editFinish" class="btn btn-default" onclick="singleChoiceEditFinish(${problem.length})">完成编辑</button>
-        </div>
-      </div>
-      <div class="bottom2" style="display: none;">
-        
-      </div>
-    </div>
-  `
 }
