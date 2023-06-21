dedbdb5580a4349290ab2b07b1318c6f4953f046
AkagawaTsurunaki <965145619@qq.com>
2023-06-21 15:50:24 +0800

    feat: 根据实体类创建对应的mapper

---
+++ src/main/java/com/github/akagawatsurunaki/ankeito/mapper/answer/ResponseSheetDetailMapper.java
@@ -0,0 +1,9 @@
+package com.github.akagawatsurunaki.ankeito.mapper.answer;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.github.akagawatsurunaki.ankeito.entity.answer.ResponseSheetDetail;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ResponseSheetDetailMapper extends BaseMapper<ResponseSheetDetail> {
+}
---
+++ src/main/java/com/github/akagawatsurunaki/ankeito/mapper/answer/ResponseSheetMapper.java
@@ -0,0 +1,9 @@
+package com.github.akagawatsurunaki.ankeito.mapper.answer;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.github.akagawatsurunaki.ankeito.entity.answer.ResponseSheet;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ResponseSheetMapper extends BaseMapper<ResponseSheet> {
+}
