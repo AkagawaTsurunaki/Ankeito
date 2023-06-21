eb6176e3e45de18525697b0ed2f9aa4c62977d1a
AkagawaTsurunaki <965145619@qq.com>
2023-06-21 16:06:10 +0800

    feat: 实现了 根据指定的问卷 ID 查询所有的答卷 ResponseSheet

--- src/main/java/com/github/akagawatsurunaki/ankeito/mapper/answer/ResponseSheetMapper.java
+++ src/main/java/com/github/akagawatsurunaki/ankeito/mapper/answer/ResponseSheetMapper.java
@@ -1,9 +1,17 @@
 package com.github.akagawatsurunaki.ankeito.mapper.answer;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.github.akagawatsurunaki.ankeito.entity.answer.ResponseSheet;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.lang.NonNull;
+
+import java.util.List;
 
 @Mapper
 public interface ResponseSheetMapper extends BaseMapper<ResponseSheet> {
+    default List<ResponseSheet> selectByQnnreId(@NonNull String qnnreId) {
+        return selectList(Wrappers.<ResponseSheet>query().lambda().eq(ResponseSheet::getQnnreId, qnnreId));
+    }
+
 }
