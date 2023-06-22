954fb6b6c704098a93df9bdb3b6faf3c37a68014
AkagawaTsurunaki <965145619@qq.com>
2023-06-23 01:11:33 +0800

    feat: 实现了前端问卷的可视化表格的显示

--- src/main/java/com/github/akagawatsurunaki/ankeito/api/dto/InnerOption.java
+++ src/main/java/com/github/akagawatsurunaki/ankeito/api/dto/InnerOption.java
@@ -1,8 +1,12 @@
 package com.github.akagawatsurunaki.ankeito.api.dto;
 
 import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
 
 @Builder
+@Data
+@Jacksonized
 public class InnerOption {
     Integer optionId;
     String optionContent;
--- src/main/resources/static/pages/statisticChart/index.js
+++ src/main/resources/static/pages/statisticChart/index.js
@@ -1,79 +1,82 @@
+let questionStatisticDTOList
+
 onload = () => {
-}
+    $util.getPageParam('selectedQnnreId')
+    $util.getPageParam('selectedQnnreId')
+    let param = {
+        qnnreId: $util.getPageParam('selectedQnnreId')
+    }
 
+    $.ajax({
+        url: '/statistic/getQuestionStatistic', // 接口地址
+        type: 'POST',
+        data: JSON.stringify(param),
+        dataType: "json",
+        contentType: "application/json",
+        success(res) {
+            if (res.code === '666') {
+                questionStatisticDTOList = [
+                    {
+                        questionId,
+                        questionName,
+                        questionCount,
+                        optionList: {
+                            optionId,
+                            optionContent,
+                            percent,
+                            count,
+                        }
+                    }
+                ] = res.data
 
-const fun = (index) => {
-    let ele = `
-    <div class="question" id="question${question.id}" data-type="1" data-problemIndex="${question.id}">
-        <div class="top">
-            <span class="question-title">第${index}题</span>
-            <span class="question-title" id="questionTitle">${question.content}</span>
-            <span class="must-answer" id="mustAnswer">${question.required === 'REQUIRED' ? '必答题' : '非必答题'}</span>
-        </div>
-        <div class="bottom2" style="display: inline;" >
-
-        </div>
-    </div>
-    `
-
-    $('#problem').append(ele)
-    questionDTO.optionList.map(optionDTO => {
-        $(`#question${question.id} .bottom2`).append(`
-      <div style="display: flex; align-items: center;">
-        <label class="radio-inline">
-          <input type="radio" id="radio-${question.id}-${optionDTO.option.id}" name="radio-${question.id}" ${optionDTO.selected ? 'checked' : ''}>${optionDTO.option.content}
-        </label>
-      </div>
-    `)
+                questionStatisticDTOList.forEach(
+                    questionStatisticDTO => {
+                        table(questionStatisticDTO)
+                    }
+                )
+            }
+            alert(res.message)
+        }
     });
+
 }
 
-const table = () => {
+// const fun = (questionId, questionName) => {
+//     let ele = `
+//     <div class="question" id="question${questionId}" data-type="1" data-problemIndex="${questionId}">
+//         <div class="top">
+//             <span class="question-title">第${questionId + 1}题</span>
+//             <span class="question-title" id="questionTitle">${questionName}</span>
+//         </div>
+//         <div class="bottom2" style="display: inline;" >
+//
+//         </div>
+//     </div>
+//     `
+//
+//     $('#problem').append(ele)
+//     questionDTO.optionList.map(optionDTO => {
+//         $(`#question${question.id} .bottom2`).append(`
+//       <div style="display: flex; align-items: center;">
+//         <label class="radio-inline">
+//           <input type="radio" id="radio-${question.id}-${optionDTO.option.id}" name="radio-${question.id}" ${optionDTO.selected ? 'checked' : ''}>${optionDTO.option.content}
+//         </label>
+//       </div>
+//     `)
+//     });
+// }
+
+const table = (questionStatisticDTO) => {
+    let questionId = questionStatisticDTO.questionId
 
     let ele = `
-    <div class="problem" id="problem">
-        <table class="table table-bordered table-striped">
-            <tr>
+    <div class="problem" id="problem${questionId}">
+        <table id="table-question-${questionId}" class="table table-bordered table-striped">
+            <tr id="tr${questionId}">
                 <th>选项</th>
                 <th>小计</th>
                 <th>比例</th>
             </tr>
-            <tr>
-                <td>选项1</td>
-                <td>66</td>
-                <td>
-                    <div class="progress-bar">
-                        <div class="progress-fill" style="width: 15.07%;"></div>
-                    </div>
-                    15.07%
-                </td>
-            </tr>
-            <tr>
-                <td>选项2</td>
-                <td>302</td>
-                <td>
-                    <div class="progress-bar">
-                        <div class="progress-fill" style="width: 68.95%;"></div>
-                    </div>
-                    68.95%
-                </td>
-            </tr>
-            <tr>
-                <td>选项3</td>
-                <td>70</td>
-                <td>
-                    <div class="progress-bar">
-                        <div class="progress-fill" style="width: 15.98%;"></div>
-                    </div>
-                    15.98%
-                </td>
-            </tr>
-            <tr>
-                <td>本题有效填写人次</td>
-                <td>438</td>
-                <td></td>
-            </tr>
-
         </table>
         <div style="text-align: right">
             <button type="button" class="btn btn-default">表格</button>
@@ -83,7 +86,40 @@ const table = () => {
             <button type="button" class="btn btn-default">条形</button>
             <button type="button" class="btn btn-default">折线</button>
         </div>
-        <div id="chartContainer" style="width: 600px; height: 400px;"></div>
+<!--        <div id="chartContainer" style="width: 600px; height: 400px;"></div>-->
     </div>
     `
+
+    $(`#problem`).append(ele)
+
+    questionStatisticDTO.optionList.forEach(
+        option => {
+            let ele =
+                `
+            <tr>
+                <td>${option.optionContent}</td>
+                <td>${option.count}</td>
+                <td>
+                    <div class="progress-bar">
+                        <div class="progress-fill" style="width: ${option.percent * 100}%;"></div>
+                    </div>
+                    ${option.percent * 100}%
+                </td>
+            </tr>
+            `
+            $(`#tr${questionId}`).after(ele)
+        }
+    )
+
+    let questionCountEle = `
+            <tr>
+                <td>本题有效填写人次</td>
+                <td>${questionStatisticDTO.questionCount}</td>
+                <td></td>
+            </tr>
+    `
+    $(`#table-question-${questionId} tr:last`).after(questionCountEle)
+
+
+
 }
