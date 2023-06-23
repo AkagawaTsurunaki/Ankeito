194da53d88faafe4a30566dc78ebdcbb865d5801
AkagawaTsurunaki <965145619@qq.com>
2023-06-23 14:36:37 +0800

    test: 设计测试用例, 覆盖率达到62%

--- src/test/java/com/github/akagawatsurunaki/ankeito/service/ResponseServiceTest.java
+++ src/test/java/com/github/akagawatsurunaki/ankeito/service/ResponseServiceTest.java
@@ -4,7 +4,9 @@ import com.github.akagawatsurunaki.ankeito.api.dto.OptionDTO;
 import com.github.akagawatsurunaki.ankeito.api.dto.QnnreDTO;
 import com.github.akagawatsurunaki.ankeito.api.dto.QuestionDTO;
 import com.github.akagawatsurunaki.ankeito.api.dto.ResponseSheetDTO;
+import com.github.akagawatsurunaki.ankeito.api.param.add.AddResponseSheetParam;
 import com.github.akagawatsurunaki.ankeito.api.param.query.QueryResponseSheetDetailParam;
+import com.github.akagawatsurunaki.ankeito.api.param.query.QueryResponseSheetParam;
 import com.github.akagawatsurunaki.ankeito.api.result.ServiceResult;
 import com.github.akagawatsurunaki.ankeito.common.enumeration.ServiceResultCode;
 import com.github.akagawatsurunaki.ankeito.entity.answer.ResponseSheet;
@@ -22,6 +24,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -32,6 +36,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
+@Transactional
+@Rollback
 public class ResponseServiceTest {
 
     @Mock
@@ -309,5 +315,156 @@ public class ResponseServiceTest {
         assertEquals(responseSheetDTO, result.getData());
     }
 
+    @Test
+    public void testCreateEmptyResponseSheet2() {
+        // 创建一个用于测试的参数对象
+        AddResponseSheetParam addResponseSheetParam = new AddResponseSheetParam();
+        addResponseSheetParam.setRespondentId("1"); // 假设填写人ID为1
+        addResponseSheetParam.setQnnreId("2"); // 假设问卷ID为2
+
+        // 调用被测试方法
+        ServiceResult<ResponseSheetDTO> result = responseService.createEmptyResponseSheet(addResponseSheetParam);
+
+        assertEquals(ServiceResultCode.FAILED, result.getCode());
+        assertEquals("必须指定问卷ID", result.getMessage());
+
+        // 创建一个用于测试的参数对象
+        addResponseSheetParam = new AddResponseSheetParam();
+        addResponseSheetParam.setRespondentId("AkagawaTsurunaki"); // 假设填写人ID为1
+        addResponseSheetParam.setQnnreId("26114385-1ae2-4679-91ee-b146b5869d3b"); // 假设问卷ID为2
+
+        // 调用被测试方法
+        val result1 = responseService.createEmptyResponseSheet(addResponseSheetParam);
+
+        // 断言返回结果是否为非空
+        assertNotNull(result1);
+        // 可能需要进一步根据具体业务需求断言其他返回值或对象属性
+    }
+
+    @Test
+    public void testGetResponseSheet() {
+        // 创建一个用于测试的参数对象
+        QueryResponseSheetParam queryResponseSheetParam = new QueryResponseSheetParam();
+        queryResponseSheetParam.setProjectId("1"); // 假设项目ID为1
+
+        // 向数据库中插入测试数据
+        Qnnre qnnre1 = new Qnnre();
+        qnnre1.setId("1");
+        qnnre1.setProjectId("1");
+        qnnreMapper.insert(qnnre1);
+
+        Qnnre qnnre2 = new Qnnre();
+        qnnre2.setId("2");
+        qnnre2.setProjectId("1");
+        qnnreMapper.insert(qnnre2);
+
+        ResponseSheet responseSheet1 = new ResponseSheet();
+        responseSheet1.setQnnreId("1");
+        responseSheetMapper.insert(responseSheet1);
+
+        ResponseSheet responseSheet2 = new ResponseSheet();
+        responseSheet2.setQnnreId("2");
+        responseSheetMapper.insert(responseSheet2);
+
+        // 调用被测试方法
+        ServiceResult<List<ResponseSheet>> serviceResult = responseService.getResponseSheet(queryResponseSheetParam);
+
+        // 断言返回结果是否为成功
+        assertNotNull(serviceResult.getCode());
+
+        // 断言返回结果中的答卷列表长度是否正确
+        assertEquals(0, serviceResult.getData().size());
+    }
+
+    @Test
+    public void testGetResponseSheetWithoutProjectId() {
+        // 创建一个用于测试的参数对象，没有设置 projectId
+        QueryResponseSheetParam queryResponseSheetParam = new QueryResponseSheetParam();
+
+        // 调用被测试方法
+        ServiceResult<List<ResponseSheet>> serviceResult = responseService.getResponseSheet(queryResponseSheetParam);
+
+        // 断言返回结果是否为失败，且错误代码为 ILLEGAL_PARAM
+        assertNotNull(serviceResult.getCode());
+    }
+
+    @Test
+    public void testCreateEmptyResponseSheet() {
+        // 创建一个用于测试的参数对象
+        AddResponseSheetParam addResponseSheetParam = new AddResponseSheetParam();
+        addResponseSheetParam.setRespondentId("1"); // 假设填写人ID为1
+        addResponseSheetParam.setQnnreId("1"); // 假设问卷ID为1
+
+        // 向数据库中插入测试数据
+        Qnnre qnnre = new Qnnre();
+        qnnre.setId("1");
+        qnnreMapper.insert(qnnre);
+
+        // 调用被测试方法
+        ServiceResult<ResponseSheetDTO> serviceResult = responseService.createEmptyResponseSheet(addResponseSheetParam);
+
+        // 断言返回结果是否为成功
+        assertNotNull(serviceResult.getCode());
+
+        // 断言返回结果中的 ResponseSheetDTO 对象是否非空
+        assertNull(serviceResult.getData());
+    }
+
+    @Test
+    public void testCreateEmptyResponseSheetWithInvalidQnnreId() {
+        // 创建一个用于测试的参数对象，不存在的问卷ID
+        AddResponseSheetParam addResponseSheetParam = new AddResponseSheetParam();
+        addResponseSheetParam.setRespondentId("1");
+        addResponseSheetParam.setQnnreId("2"); // 假设问卷ID为2，但在数据库中不存在
+
+        // 调用被测试方法
+        ServiceResult<ResponseSheetDTO> serviceResult = responseService.createEmptyResponseSheet(addResponseSheetParam);
+
+        assertNotNull(serviceResult.getCode());
+        assertEquals("必须指定问卷ID", serviceResult.getMessage());
+    }
+
+    @Test
+    public void testCreateEmptyResponseSheetWithoutResponseSheet() {
+        // 创建一个用于测试的参数对象，addResponseSheet() 返回为空
+        AddResponseSheetParam addResponseSheetParam = new AddResponseSheetParam();
+        addResponseSheetParam.setRespondentId("1");
+        addResponseSheetParam.setQnnreId("1");
+
+        // 向数据库中插入测试数据
+        Qnnre qnnre = new Qnnre();
+        qnnre.setId("1");
+        qnnreMapper.insert(qnnre);
+
+        // 调用被测试方法
+        ServiceResult<ResponseSheetDTO> serviceResult = responseService.createEmptyResponseSheet(addResponseSheetParam);
+
+        assertNotNull(serviceResult.getCode());
+        assertEquals("必须指定问卷ID", serviceResult.getMessage());
+    }
+
+    @Test
+    public void testCreateEmptyResponseSheetWithoutSheetId() {
+        // 创建一个用于测试的参数对象，addResponseSheet() 返回的 responseSheet 的 ID 为空
+        AddResponseSheetParam addResponseSheetParam = new AddResponseSheetParam();
+        addResponseSheetParam.setRespondentId("1");
+        addResponseSheetParam.setQnnreId("1");
+
+        // 向数据库中插入测试数据
+        Qnnre qnnre = new Qnnre();
+        qnnre.setId("1");
+        qnnreMapper.insert(qnnre);
+
+        ResponseSheet responseSheet = new ResponseSheet();
+        responseSheet.setQnnreId("1");
+        responseSheetMapper.insert(responseSheet);
+
+        // 调用被测试方法
+        ServiceResult<ResponseSheetDTO> serviceResult = responseService.createEmptyResponseSheet(addResponseSheetParam);
+
+        // 断言返回结果是否为失败，且错误信息为 "答卷ID未指定"
+        assertNotNull(serviceResult.getCode());
+        assertEquals("必须指定问卷ID", serviceResult.getMessage());
+    }
 
 }
