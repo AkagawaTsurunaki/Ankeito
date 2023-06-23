0cc0d7256bc2de3c8f5a0e52091990e60f7808a0
AkagawaTsurunaki <965145619@qq.com>
2023-06-23 15:07:38 +0800

    test: 设计测试用例, 覆盖率达到66%

--- src/main/java/com/github/akagawatsurunaki/ankeito/api/param/add/AddQuestionParam.java
+++ src/main/java/com/github/akagawatsurunaki/ankeito/api/param/add/AddQuestionParam.java
@@ -2,9 +2,11 @@ package com.github.akagawatsurunaki.ankeito.api.param.add;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
 
 @Data
 @Builder
+@Jacksonized
 public class AddQuestionParam {
     String qnnreId;
     Integer index;
--- src/main/java/com/github/akagawatsurunaki/ankeito/api/param/modify/ModifyQnnreParam.java
+++ src/main/java/com/github/akagawatsurunaki/ankeito/api/param/modify/ModifyQnnreParam.java
@@ -2,9 +2,13 @@ package com.github.akagawatsurunaki.ankeito.api.param.modify;
 
 import com.github.akagawatsurunaki.ankeito.api.param.add.AddOptionParam;
 import com.github.akagawatsurunaki.ankeito.api.param.add.AddQuestionParam;
+import lombok.Builder;
 import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
 
 @Data
+@Builder
+@Jacksonized
 public class ModifyQnnreParam {
 
     AddQuestionParam[] addQuestionParams;
