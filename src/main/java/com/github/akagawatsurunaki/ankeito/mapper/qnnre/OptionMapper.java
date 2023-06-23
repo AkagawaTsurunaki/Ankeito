236155ed51a3c3fed89b39f8efb431867fe67594
AkagawaTsurunaki <965145619@qq.com>
2023-06-23 16:12:18 +0800

    fix: 修复了题目串题的错误

--- src/main/java/com/github/akagawatsurunaki/ankeito/mapper/qnnre/OptionMapper.java
+++ src/main/java/com/github/akagawatsurunaki/ankeito/mapper/qnnre/OptionMapper.java
@@ -3,7 +3,6 @@ package com.github.akagawatsurunaki.ankeito.mapper.qnnre;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.github.akagawatsurunaki.ankeito.entity.qnnre.Option;
-import com.github.akagawatsurunaki.ankeito.entity.qnnre.Question;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.lang.NonNull;
 
@@ -15,4 +14,10 @@ public interface OptionMapper extends BaseMapper<Option> {
     default List<Option> selectByQuestionId(@NonNull Integer questionId) {
         return selectList(Wrappers.<Option>query().lambda().eq(Option::getQuestionId, questionId));
     }
+
+    default List<Option> selectByQnnreIdAndQuestionId(@NonNull String qnnreId, @NonNull Integer questionId) {
+        return selectList(Wrappers.<Option>query().lambda().eq(Option::getQnnreId, qnnreId).eq(Option::getQuestionId,
+                questionId));
+    }
+
 }
